b93b9399985b7007bd94880ac5c9062a1272591e
Leighton <63763567+leighton-0@users.noreply.github.com>
2023-11-15 10:32:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -355,7 +355,7 @@ nordvpn(){
     sudo apt-get install /home/kali/Downloads/nordvpn-release_1.0.0_all.deb
     sudo apt-get update -y
     sudo apt-get install nordvpn -y
-    apt install -y ./nordvpn-release_1.0.0_all.deb
+    # apt install -y ./nordvpn-release_1.0.0_all.deb
     #fi
     # token 30 day expiring - 11 December
     # e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403
