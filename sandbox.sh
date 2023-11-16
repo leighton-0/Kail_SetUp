8249b8344258d801066b2cacbb70778bf995eb00
Leighton <63763567+leighton-0@users.noreply.github.com>
2023-11-16 08:42:53 +0000

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -12,11 +12,11 @@ printf "  🔧  install & logon to nordvpn\n" | tee -a script.log
     wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
     #sudo apt-get install /home/kali/Downloads/nordvpn-release_1.0.0_all.deb
     fi
-    printf "  🔧  nordvpn already installed\n" 
+    #printf "  🔧  nordvpn already installed\n" 
     sleep 5
-    << ////
     sudo apt-get update -y
     sudo apt-get install nordvpn -y
+    << ////
     apt install -y ./nordvpn-release_1.0.0_all.deb
     # token 30 day expiring - 11 December
     # e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403
