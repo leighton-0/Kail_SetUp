ffb191b212b4e6610501b049ea5b6146403c7745
Leighton <63763567+leighton-0@users.noreply.github.com>
2023-11-16 05:37:21 +0000

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-run using   https://raw.githubusercontent.com/leighton-0/Kail_SetUp/main/sandbox.sh | bash
+run using  curl https://raw.githubusercontent.com/leighton-0/Kail_SetUp/main/sandbox.sh | bash
 
 printf "  🔧  install & logon to nordvpn\n" | tee -a script.log
     # ref:- https://www.ceos3c.com/linux/install-nordvpn-linux/
     # if [ ! -f '~/nordvpn*']
     # then
     cd /home/kali/Downloads
-    # if [ ! -f './nordvpn*']
-    # then
+    if [ ! -f './nordvpn*']
+    then
     wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-    # wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-    cd ~
     sudo apt-get install /home/kali/Downloads/nordvpn-release_1.0.0_all.deb
-    #sudo apt-get update -y
-    #sudo apt-get install nordvpn -y
-    #cd ~
-    # apt install -y ./nordvpn-release_1.0.0_all.deb
-    # fi
+    sudo apt-get update -y
+    sudo apt-get install nordvpn -y
+    cd ~
+    apt install -y ./nordvpn-release_1.0.0_all.deb
+    fi
     # token 30 day expiring - 11 December
     # e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403
-    nordvpn login --token e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403
+    nordvpn login --token e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e
     sleep $s
     nordvpn connect Double VPN
     sleep $s
