6b21e03f000f0e1e615f4b9700f04b7c0003885c
Leighton <63763567+leighton-0@users.noreply.github.com>
2023-11-17 08:46:43 +0000

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -7,10 +7,6 @@ cat <<EOF >/etc/apt/sources.list
 deb https://http.kali.org/kali kali-rolling main non-free contrib
 EOF
 
-#token=e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403
-#echo "enter nordvpn token eg e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403"
-read -ip "e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403" token
-
 printf "  🔧  install & logon to nordvpn\n" | tee -a script.log
     s=5
     # ref:- https://www.ceos3c.com/linux/install-nordvpn-linux/
@@ -19,10 +15,7 @@ printf "  🔧  install & logon to nordvpn\n" | tee -a script.log
     sudo apt install /home/kali/Downloads/nordvpn-release_1.0.0_all.deb
     sudo apt update -y
     sudo apt install nordvpn -y
-    # token 30 day expiring - 11 December
-    # e9f2ab4ec9525094e607b4b25633c2e890e60c200af048a5ef8202dd47b8386e0403
-    # read -n 1 -p "input token" 
-    nordvpn login --token $token
+    nordvpn login --token $TOKEN
     sleep $s
     nordvpn connect Double VPN
     sleep $s
