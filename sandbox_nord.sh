3bd416753117a91cd1fd61a1a6a27856658cb931
Leighton <63763567+leighton-0@users.noreply.github.com>
2023-11-20 19:58:24 +0000

    Update sandbox_nord.sh

--- sandbox_nord.sh
+++ sandbox_nord.sh
@@ -11,7 +11,6 @@ deb https://http.kali.org/kali kali-rolling main non-free contrib
 EOF
 
 printf "  🔧  install & logon to nordvpn\n" | tee -a script.log
-    s=5
     # ref:- https://www.ceos3c.com/linux/install-nordvpn-linux/
     cd /home/kali/Downloads
     wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
@@ -19,14 +18,14 @@ printf "  🔧  install & logon to nordvpn\n" | tee -a script.log
     sudo apt update -y
     sudo apt install nordvpn -y
     nordvpn login --token $TOKEN
-    sleep $s
+    
     nordvpn connect Double VPN
-    sleep $s
+  
     nordvpn s killswitch on
     # #nordvpn connect "#656"
     # #nordvpn -c -n "United States #3710"
     # nordvpn connect --group Dedicated_IP Germany
-    # sleep 10
+   
     nordvpn status
-    sleep 10
+    sleep 5
     printf "  🔧  THE END\n" 
