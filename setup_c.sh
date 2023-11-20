c8db8be426c2ad6259ff29d3ef90eeef09601709
Leighton <63763567+leighton-0@users.noreply.github.com>
2023-11-20 07:55:37 +0000

    Update setup_c.sh

--- setup_c.sh
+++ setup_c.sh
@@ -336,7 +336,7 @@ gedit() {
     printf "  ⏳  Install Gedit\n" | tee -a script.log
     apt install gedit -y
 }
-
+<< \\\\
 terminator() {
     printf "  ⏳  Install & Set up Terminator\n" | tee -a script.log
     apt install terminator
@@ -344,7 +344,7 @@ terminator() {
     wget -P '.config/terminator/' https://raw.githubusercontent.com/leighton-0/5_Terminator_config/main/main
     #curl -k -s https://raw.githubusercontent.com/leighton-0/kali-setup/master/kali-setup-script.sh | bash
 }
-
+\\\\
 deluge() {
      printf "  ⏳  Install deluge\n" | tee -a script.log
      sleep $s
